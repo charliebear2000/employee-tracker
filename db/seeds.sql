11b7958beb2d1ecfe2ac2873f13189ec13398fd4
Leah <amelia_rawls@ccpsnet.net>
2022-05-13 10:24:59 -0400

    added schema and seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO department (name)
+VALUES
+   ('Engineering'),
+   ('Technology'),
+   ('Customer Care'),
+   ('Product'),
+   ('Data Science');
+
+INSERT INTO roles (title, salary, department_id, )
+VALUES
+   ('Software Engineer', 85000, 1),
+   ('Data Engineer', 80000, 1),
+   ('Cyber Engineer', 90000, 1),
+   ('Cloud Engineer', 95000, 1),
+   ('Video Technician', 65000, 2),
+   ('Cyber Technician', 67000, 2),
+   ('IT Specialist', 92000, 2),
+   ('Complaints Coordinator', 58000, 3),
+   ('Customer Service Coordinator', 52000, 3),
+   ('Collections Coordinator', 45000, 3),
+   ('Product Manager', 70000, 4),
+   ('Customer Acquisitions', 56000, 4),
+   ('UX/UI Designer', 78000, 4),
+   ('Data Scientist', 92000, 5),
+   ('Quantitative Analyst', 120000, 5);
+
+   INSERT INTO   employee (first_name, last_name, job_title, department_id)
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+   {
+      host: 'localhost',
+      user: 'root',
+      password: 'Cloverhill2022$',
+      database: 'business'
+   },
+   console.log('Connected to the business database.')
+);
+
+
+
+app.listen(PORT, () => {
+   console.log(`Server running on ${PORT}`);
+});
