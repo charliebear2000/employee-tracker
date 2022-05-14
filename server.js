849efe6cc1d9efcdaf2b6dc3a33d83c97688c5a3
Leah <amelia_rawls@ccpsnet.net>
2022-05-14 17:07:28 -0400

    added initial prompt

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const mysql = require('mysql2');
+const Connection = require('mysql2/typings/mysql/lib/Connection');
 
 // connect to database
 const db = mysql.createConnection(
@@ -18,7 +19,52 @@ const db = mysql.createConnection(
 db.connect(err => {
    if (err) throw err;
    console.log('Database connected.');
+   userInput();
 
    });
 
+// inquirer prompts
+const userInput = () => {
+   inquirer.prompt ([
+      {
+      type: 'list',
+      name: 'mainMenu',
+      message: 'What would you like to do?',
+      choices: ['View all departments',
+               'View all roles',
+               'View all employees',
+               'Add a department',
+               'Add a role',
+               'Add an employee',
+               'Update an employee role',
+               'Exit application']
+      }
+   ])
+   .then(answer => {
+      if (answer.userInput === 'View all departments') {
+         viewDepartments();
+      }
+      if (answer.userInput === 'View all roles') {
+         viewRoles();
+      }
+      if (answer.userInput === 'View all employees') {
+         viewEmployees();
+      }
+      if (answer.userInput === 'Add a department') {
+         addDepartment();
+      }
+      if (answer.userInput === 'Add a role') {
+         addRole();
+      }
+      if (answer.userInput === 'Add an employee') {
+         addEmployee();
+      }
+      if (answer.userInput === 'Update an employee role') {
+         employeeRole();
+      }
+      if (answer.userInput === 'Exit application') {
+         db.end();
+      }
+   });
+};
 
